f57ce758c77cfc815adf6c5694bd961f7b52d916
JIN <edgepps@163.com>
2020-05-03 20:34:28 +0900

    Project 3

--- Project 3/DatePicker.js
+++ Project 3/DatePicker.js
@@ -1,13 +1,13 @@
 'use strict';
 class DatePicker {
-    constructer(id, callback) {
+    constructor(id, callback) {
         this.id = id;
         this.callback = callback;
     }
 
     render(date) {
         var formatdate = document.getElementById(this.id);
-        formatdate.innerHTML(this.createcal(date));
+        formatdate.appendChild(this.createcal(date));
     }
 
     createcal(date) {
@@ -45,24 +45,27 @@ class DatePicker {
         var dates = document.createElement("tbody");
         // Create controls
         var prev = document.createElement("span");
+        prev.width = 100;
         prev.innerHTML = "<";
         prev.setAttribute("id","prev");
         var next = document.createElement("span");
+        next.width = 100;
         next.innerHTML = ">";
         next.setAttribute("id","next");
-        prev.addEventListener("click", (function(){
+        prev.addEventListener("click", () => {
             table.remove();
             date.setMonth(date.getMonth() - 1);
             this.render(date);
-        })());
-        next.addEventListener("click", (function(){
+        });
+        next.addEventListener("click", () => {
             table.remove();
-            date.setMonth(date.getMonth + 1);
+            date.setMonth(date.getMonth() + 1);
             this.render(date);
-        })());
+        });
         //Week row
         for(let i = 0 ; i < _week.length ; i++){
             cell = document.createElement("th");
+            cell.width = 50;
             cell.innerHTML = _week[i];
             first_line.appendChild(cell);
         }
@@ -72,11 +75,14 @@ class DatePicker {
         // filliing in the calendar
 
         //First line
-        var curr_month_info = new Date(date.getFullYear(), date.getMonth, 1);
+        var curr_month_info = new Date(date.getFullYear(), date.getMonth(), 1);
+        console.log("Month: ", curr_month_info);
         var first_day = curr_month_info.getDay();
+        console.log("First day:",first_day);
         var last_max_date = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
         for(let i = 0 ; i < 7 ; i++){
             cell = document.createElement("td");
+            cell.width = 50;
             if(i < first_day){
                 cell.setAttribute("class", "notinclude");
                 cell.innerHTML = last_max_date -first_day +i + 1;
@@ -96,9 +102,12 @@ class DatePicker {
         week = document.createElement("tr");
         var second_start = 7 - first_day + 1;
         var curr_max_date = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+        console.log("Current Month date: ", curr_max_date);
         var counter = 0;
         while ( second_start <= curr_max_date){
             cell = document.createElement("td");
+            cell.width = 50;
+            cell.setAttribute("class", "include");
             cell.innerHTML = second_start;
             week.appendChild(cell);
             second_start++;
@@ -106,6 +115,7 @@ class DatePicker {
             if(counter === 7){
                 dates.appendChild(week);
                 week = document.createElement("tr");
+                counter = 0;
             }
         }
         //Last line of the calendar
@@ -113,6 +123,7 @@ class DatePicker {
         var last_day = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDay();
         while(last_day !== 6) {
             cell = document.createElement("td");
+            cell.width = 50;
             cell.setAttribute("class", "notinclude");
             cell.innerHTML = next_date_filling;
             next_date_filling++;
--- Project 3/TableTemplate.js
+++ Project 3/TableTemplate.js
@@ -1,44 +1,44 @@
-// class TableTemplate {
-//     static fillIn(id, dict, columnName) {
-//         var row_list = document
-//             .getElementById(id)
-//             .getElementsByTagName("dates")
-//             .getElementsByTagName("tr");
-        
-//             var is_header = true;
-
-//     }
-// }
-
 class TableTemplate {
-    static fillIn(id, dict, column_name) {
-      var row_list = document
-        .getElementById(id)
-        .getElementsByTagName("tbody")[0]
-        .getElementsByTagName("tr");
-      var is_header = true;
-      var td_list, text, col_to_replace;
-  
-      for (let row of row_list) {
-        td_list = row.getElementsByTagName("td");
-        if (is_header) {
-          for (let i = 0; i < td_list.length; i++) {
-            text = td_list[i].innerHTML.slice(2, -2);
-            td_list[i].innerHTML = text in dict ? dict[text] : " ";
-            console.log(text, column_name);
-            if (dict[text] === column_name) {
-              col_to_replace = i;
+    static fillIn(id, dict, columnName) {
+        var row_list = document.getElementById(id).getElementsByTagName("tbody")[0].getElementsByTagName("tr");
+        
+        var trlen = row_list.length;
+        console.log("Rows of the table: " , trlen);
+        
+        if(columnName === undefined){
+            // let head = row_list[0];
+            // let tprocessor = new Cs142TemplateProcessor(head.innerHTML);
+            // head.innerHTML = tprocessor.fillIn(dict);
+            // console.log("Current header: ", head);
+            for(let i = 0 ; i < row_list.length ; i++) {
+                let innerprocessor = new Cs142TemplateProcessor(row_list[i].innerHTML);
+                console.log("Current html value: ", row_list[i].innerHTML);
+                row_list[i].innerHTML = innerprocessor.fillIn(dict);
             }
-          }
-          if (column_name !== undefined) {
-            is_header = false;
-          }
         } else {
-          text = td_list[col_to_replace].innerHTML.slice(2, -2);
-          td_list[col_to_replace].innerHTML = text in dict ? dict[text] : " ";
+            let head = row_list[0];
+            let tprocessor = new Cs142TemplateProcessor(head.innerHTML);
+            head.innerHTML = tprocessor.fillIn(dict);
+
+            var found = -1;
+            var index = 0;
+            while(found === -1) {
+                if(head.cells[index].innerHTML === columnName){
+                    found = 1;
+                    break;
+                }
+                else {
+                    index++;
+                }
+            }
+            for(let i = 1 ; i < row_list.length ; i++) {
+                let inner = row_list[i].cells[index];
+                let innerprocessor = new Cs142TemplateProcessor(inner.innerHTML);
+                inner.innerHTML = innerprocessor.fillIn(dict);
+            }
         }
-      }
-  
-      document.getElementById(id).setAttribute("style", "visibility:visible");
+        // document.getElementById(id).style.visibility = "visible";
+        document.getElementById(id).setAttribute("style", "visibility:visible");
     }
-  }
+}
+
